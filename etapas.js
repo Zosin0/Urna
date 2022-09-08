c0a917ce32de8cb3b06b479a085f62370b7874b1
Zosin0 <lucaszoser@sempreceub.com>
2022-09-08 19:34:59 -0300

    quase pronta

---
+++ etapas.js
@@ -0,0 +1,120 @@
+let etapas = [
+    {
+        titulo: 'Deputado Federal',
+        numeros: '4',
+        candidatos: [
+            {
+                numero: '2345',
+                name: 'Lucas Zoser',
+                partido: 'Zosin',
+                fotos: [
+                    { url: 'lucasz.jpg', legenda: 'Deputado Federal', small: false }
+                ]
+            },
+            {
+                numero: '6789',
+                name: 'Leo Sands',
+                partido: 'DEF',
+                fotos: [
+                    { url: 'leos.jpg', legenda: 'Deputado Federal' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Deputado Estadual',
+        numeros: '5',
+        candidatos: [
+            {
+                numero: '23456',
+                name: 'Lucas Zoser',
+                partido: 'ZOSER',
+                fotos: [
+                    { url: 'lucasz', legenda: 'Deputado Estadual', small: false }
+                ]
+            },
+            {
+                numero: '67891',
+                name: 'Leo Sands',
+                partido: 'SANDS',
+                fotos: [
+                    { url: 'leos', legenda: 'Deputado Estadual' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Senador',
+        numeros: '3',
+        candidatos: [
+            {
+                numero: '123',
+                name: 'Lucas Zoser',
+                partido: 'ZOSER',
+                fotos: [
+                    { url: 'lucasz', legenda: 'Senador', small: false }
+                ]
+            },
+            {
+                numero: '456',
+                name: 'Leo Sands',
+                partido: 'SANDS',
+                fotos: [
+                    { url: 'leos', legenda: 'Senador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Governador',
+        numeros: '2',
+        candidatos: [
+            {
+                numero: '12',
+                name: 'Pedrao',
+                partido: 'PED',
+                vice: 'Leo Sands',
+                fotos: [
+                    { url: 'pedrao', legenda: 'Governador' },
+                    { url: 'leos', legenda: 'Vice-Governador', small: true }
+                ]
+            },
+            {
+                numero: '21',
+                name: 'Sands',
+                partido: 'AREIA',
+                vice: 'Zosin',
+                fotos: [
+                    { url: 'leos', legenda: 'Governador' },
+                    { url: 'lucasz', legenda: 'Vice-Governador ', small: true }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Presidente',
+        numeros : '2',
+        candidatos: [
+            {
+                numero: '13',
+                name: 'Leo Bom',
+                partido: 'BOM',
+                vice: 'Leo Sands',
+                fotos: [
+                    { url: 'leob', legenda: 'Presidente' },
+                    { url: 'leos', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: '23',
+                name: 'Zosin',
+                partido: 'ZOSER',
+                vice: 'Pedrão',
+                fotos: [
+                    { url: 'lucasz', legenda: 'Presidente' },
+                    { url: 'pedrao', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+        ]
+    }
+]
